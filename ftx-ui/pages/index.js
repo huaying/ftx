c51bfe3dee99dca87291415e03a9a7bd57cfea1e
Huaying <royal3501@gmail.com>
2021-02-23 11:17:35 +0800

    remove horizontal padding

--- ftx-ui/pages/index.js
+++ ftx-ui/pages/index.js
@@ -20,7 +20,7 @@ function TabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && <Box p={3}>{children}</Box>}
+      {value === index && <Box py={3}>{children}</Box>}
     </div>
   );
 }
